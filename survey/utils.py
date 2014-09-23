272f467fbaa617346974bc1461ba84d3fb748a01
Bogdan Buhai <bogdan.buhai@3pillarglobal.com>
2014-09-23 16:01:51 +0300

    core app with templatetags

--- survey/utils.py
+++ survey/utils.py
@@ -1,6 +1,11 @@
 
 
 def get_first_value(q_set):
+    """Get the first value from a dict, or None if empty.
+
+    :param q_set:
+    :return:
+    """
     try:
         return q_set[0]
     except IndexError:
